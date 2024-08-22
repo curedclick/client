29ada19fd5ad94f0c31e956a8d1512413174a1b4
fahedarshad01 <fahed.arshad@ovo.com>
2024-08-22 12:05:11 +0100

    wip

---
+++ client/src/app/layout.server.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+import RootLayout from "./layout";
+
+export const metadata: Metadata = {
+  title: "KinWell Pharmacy",
+  description: "KinWell Pharmacy Portal",
+};
+
+export default RootLayout;
---
+++ client/src/app/theme/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#05173d",
+    },
+    secondary: {
+      main: "#00a2af",
+    },
+    text: {
+      primary: "#303133",
+    },
+  },
+  typography: {
+    fontFamily: "Montserrat, Roboto, Arial, sans-serif",
+  },
+});
