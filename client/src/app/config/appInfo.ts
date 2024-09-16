d33dcd4fa0426bf80719dbedf3bec0fe50ef4bfb
fahedarshad01 <fahed.arshad@ovo.com>
2024-09-16 10:09:59 +0100

    update app info

--- client/src/app/config/appInfo.ts
+++ client/src/app/config/appInfo.ts
@@ -1,8 +1,9 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "KinWell Pharmacy",
-  apiDomain: "http://localhost:4444",
-  websiteDomain: "http://localhost:4445",
+  apiDomain:
+    process.env.NEXT_PUBLIC_PHARMA_PAL_API_URL ?? "http://localhost:4444",
+  websiteDomain: process.env.NEXT_PUBLIC_PORTAL_URL ?? "http://localhost:4445",
   apiBasePath: "/auth",
   websiteBasePath: "/auth",
 };
